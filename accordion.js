c184af19dd8390de811c2e673eb7dca8e270f262
ssoheily <sam.soheil55@gmail.com>
2021-08-15 21:05:03 +0200

    work on the accardion menu mit animation view

--- accordion.js
+++ accordion.js
@@ -2,25 +2,42 @@
 
 
 
-
+//ACCORDION VIEW IN WEBPAGE
 let accordion = document.querySelector(".accordion")
 // convsert htmlCollection to Array,weil cann t work mit forEach
 Array.from(accordion.children).forEach(wrapper => {
+    if(wrapper.classList.contains("show")){
+        wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight+30 +"px"
+
+    }
     wrapper.querySelector("span").addEventListener("click", e => {
         // console.log(e.target)
         let span=e.target;
         let wrapper = span.parentElement
+        let div = span.nextElementSibling
+
         wrapper.classList.toggle("show")
+        if(wrapper.classList.contains("show")){
+            div.style.maxHeight = div.scrollHeight + 30 + "px"
+        }
+        else{
+            div.style.maxHeight= null
+        }
+        div.scrollHeight
+
+        // wenn a accordin open, next close
         Array.from(accordion.children).forEach(w => {
             if(w != wrapper){
                 w.classList.remove("show")
+                w.style.maxHeight= null
+
             }
         })
     })
 })
 
 
-
+// check internet concect
 
 let el = document.createElement("div")
 // el.textContent="hello"
