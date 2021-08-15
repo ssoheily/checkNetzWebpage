7284b3fa86b1b952b5f16b2132aca87297cb6770
ssoheily <sam.soheil55@gmail.com>
2021-08-15 23:04:49 +0200

    last step accordion

--- accordion.js
+++ accordion.js
@@ -3,59 +3,64 @@
 
 
 //ACCORDION VIEW IN WEBPAGE
-let accordion = document.querySelector(".accordion")
-// convsert htmlCollection to Array,weil cann t work mit forEach
-Array.from(accordion.children).forEach(wrapper => {
-    if(wrapper.classList.contains("show")){
-        wrapper.querySelector("div").style.maxHeight 
-        = wrapper.querySelector("div").scrollHeight + 30 +"px"
+let accordion = document.querySelectorAll(".accordion")
 
-    }
-    wrapper.querySelector("span").addEventListener("click", e => {
-        // console.log(e.target)
-        let span=e.target;
-        let wrapper = span.parentElement
-        let div = span.nextElementSibling
+    accordion.forEach(accordion => {
 
-        wrapper.classList.toggle("show")
-        if(wrapper.classList.contains("show")){
-            div.style.maxHeight = div.scrollHeight + 30 + "px"
-        }
-        else{
-            div.style.maxHeight= null
-        }
-        
+        // convsert htmlCollection to Array,weil cann t work mit forEach
+                Array.from(accordion.children).forEach(wrapper => {
+                    if(wrapper.classList.contains("show")){
+                        wrapper.querySelector("div").style.maxHeight 
+                        = wrapper.querySelector("div").scrollHeight + 30 +"px"
 
-        // wenn a accordin open, next close
-        Array.from(accordion.children).forEach(w => {
-            if(w != wrapper){
-                w.classList.remove("show")
-                w.querySelector("div").style.maxHeight = null
+                    }
+                    wrapper.querySelector("span").addEventListener("click", e => {
+                        // console.log(e.target)
+                        let span=e.target;
+                        let wrapper = span.parentElement
+                        let div = span.nextElementSibling
 
-            }
-        })
+                        wrapper.classList.toggle("show")
+                        if(wrapper.classList.contains("show")){
+                            div.style.maxHeight = div.scrollHeight + 30 + "px"
+                        }
+                        else{
+                            div.style.maxHeight= null
+                        }
+                        
+
+                        // wenn a accordin open, next close
+                        Array.from(accordion.children).forEach(w => {
+                            if(w != wrapper){
+                                w.classList.remove("show")
+                                w.querySelector("div").style.maxHeight = null
+
+                            }
+                        })
+                    })
+                })
+
+
+                // check internet concect
+
+                let el = document.createElement("div")
+                // el.textContent="hello"
+                // document.querySelector(".content").append(el)
+                document.querySelector(".content").prepend(el)
+
+
+                // addEventListener version
+                window.addEventListener('online', (event) => {
+                    el.textContent="online"
+                    el.className="alert"
+                    el.classList.add("alert-success")
+                });
+
+                // addEventListener version
+                window.addEventListener('offline', (event) => {
+                    el.textContent="offline"
+                    el.className="alert"
+                    el.classList.add("alert-danger")
+                });  
     })
-})
-
-
-// check internet concect
-
-let el = document.createElement("div")
-// el.textContent="hello"
-// document.querySelector(".content").append(el)
-document.querySelector(".content").prepend(el)
-
-
-// addEventListener version
-window.addEventListener('online', (event) => {
-    el.textContent="online"
-    el.className="alert"
-    el.classList.add("alert-success")
-});
-
-// addEventListener version
-window.addEventListener('offline', (event) => {
-    el.textContent="offline"
-    el.className="alert"
-    el.classList.add("alert-danger")
-});  
+                
